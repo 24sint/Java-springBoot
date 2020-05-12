81531f9a893598215e7c774b00e65ec9b228bec0
Sintayehu <sintemite@gmail.com>
2020-05-12 14:16:06 -0400

    some files are updated

---
+++ src/test/java/cucumberTests/Stepdefs.java
@@ -0,0 +1,35 @@
+package cucumberTests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Stepdefs {
+	private WebDriver browser;
+	
+	@Test
+	@Given("^The web browser is opened on google home")
+	
+	public void startChromeWithGoogle() throws Exception{
+		WebDriverManager.chromedriver().setup();
+		browser = new ChromeDriver();
+		browser.get("http://localhost:4200/users/list");
+		
+		WebElement el = browser.findElement(By.cssSelector("body"));
+		System.out.println(el.getText());
+
+
+//		Boolean isPageTitleOk = pageTitle.equals("Google");
+//		assertTrue(isPageTitleOk);
+		
+//		browser.close();
+		
+	}
+}
